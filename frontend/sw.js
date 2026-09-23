7ef37218a590bc217b72a1b813bec45e828729b6
Vivek <vivek27.work@gmail.com>
2026-09-23 21:32:51 +0530

    Remove local event data after Supabase migration

--- frontend/sw.js
+++ frontend/sw.js
@@ -11,7 +11,7 @@ const STATIC_ASSETS = [
   "./style.css",
   "./app.js",
   "./manifest.json",
-  "./data/events.json",
+ 
   "./posters/academic.png",
   "./posters/exams.png",
   "./posters/breaks.png",
